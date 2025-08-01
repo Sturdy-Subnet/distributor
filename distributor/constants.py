479552cf093b56a3f5012e0223891f9ab4a6521f
Syeam Bin Abdullah <49330057+Shr1ftyy@users.noreply.github.com>
2025-08-01 12:32:16 +0930

    chore: set NETUID to 10

--- distributor/constants.py
+++ distributor/constants.py
@@ -4,7 +4,7 @@ SECONDS_PER_BT_BLOCK = 12  # 12 seconds per block
 BLOCKS_PER_EPOCH = 360
 SECONDS_PER_DAY = 86400  # 24 hours * 60 minutes * 60 seconds
 SECONDS_PER_MINUTE = 60  # 60 seconds in a minute
-NETUID = 2  # netuid
+NETUID = 10  # netuid
 MIN_REWARD_THRESHOLD = (
     1e-4  # minimum reward for an LP position to be considered for distribution
 )
